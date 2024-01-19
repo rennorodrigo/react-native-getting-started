976a256699acbd669caa88dfeaa3983ce72a2c92
Rodrigo <rodrigo.renno@novaconcursos.com.br>
2024-01-19 10:08:25 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View} from 'react-native';
+
+import ImageViewer from "./src/components/ImageViewer";
+import Button from "./src/components/Button";
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer placeholderImageSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button label="Choose a photo" />
+                <Button label="Use this photo" />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
+});
